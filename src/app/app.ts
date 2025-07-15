1992d6da8aca07a133cd29fda037a66ebb66d77d
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2025-07-15 16:13:45 -0400

    Hello world

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
-  styleUrls: ['./app.css'],
+  template: ` <h1>Hello world!</h1> `,
+  styleUrls: ["./app.css"],
 })
 export class App {
-  title = 'default';
+  title = "homes";
 }
