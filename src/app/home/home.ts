baf05642f2d3e06a69a5fc0a66218c9ea3121840
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2025-07-15 16:35:51 -0400

    Search filter

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <p>home works!</p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
   `,
-  styles: ``
+  styleUrls: ["home.css"],
 })
-export class Home {
-
-}
+export class Home {}
